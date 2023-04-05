fcb23f73eb35272f567ee692d854f97b122d1341
Azat <8280770+azataiot@users.noreply.github.com>
2023-04-05 20:37:19 +0300

    finished chapter 4 for the layout

--- ch4-layout/dash_example.md
+++ ch4-layout/dash_example.md
@@ -1,2 +1,26 @@
 ![img_1.png](img_1.png)
 
+## adding interactivity
+
+```python
+dcc.RadioItems(
+        options=df['region'].unique(),
+        value='North America',
+    ),
+    dcc.Checklist(
+        options=df['region'].unique(),
+        value=['North America'],
+    ),
+    dcc.Dropdown(
+        options=df['country'].unique(),
+        value='United States',
+    ),
+    dcc.Graph(
+        figure=px.line(
+            df[df['country'] == 'United States'],
+            x='year', y='happiness_score',
+            title="United States Happiness Score"),
+    ),
+```
+![img.png](img.png)
+
--- ch4-layout/dash_example.py
+++ ch4-layout/dash_example.py
@@ -1,8 +1,21 @@
 from dash import Dash, html, dcc
 import pandas as pd
+import plotly.express as px
+
+# import the data and read with pandas
+df = pd.read_csv('world_happiness.csv')
+
+# create a figure
+figure = px.line(
+    df[df['country'] == 'United States'],
+    x='year', y='happiness_score',
+    title="United States Happiness Score",
+)
+
 app = Dash(__name__)
 
 app.layout = html.Div(children=[
+    # title and data source
     html.H1(children='World Happiness Dashboard'),
     html.P([
         'This dashboard shows the happiness score of countries around the world.',
@@ -13,6 +26,21 @@ app.layout = html.Div(children=[
             target='_blank'
         ),
     ]),
+    dcc.RadioItems(
+        options=df['region'].unique(),
+        value='North America',
+    ),
+    dcc.Checklist(
+        options=df['region'].unique(),
+        value=['North America'],
+    ),
+    dcc.Dropdown(
+        options=df['country'].unique(),
+        value='United States',
+    ),
+    dcc.Graph(
+        figure=figure,
+    ),
 ])
 
 if __name__ == '__main__':
