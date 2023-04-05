c07f0c97686ec2ef262458461417bf59440fe64d
Azat <8280770+azataiot@users.noreply.github.com>
2023-04-05 18:16:15 +0300

    ch4 html components tried out

---
+++ ch4-layout/dash_example.py
@@ -0,0 +1,19 @@
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='World Happiness Dashboard'),
+    html.P([
+        'This dashboard shows the happiness score of countries around the world.',
+        html.Br(),
+        html.A(
+            'Data source',
+            href='https://worldhappiness.report/',
+            target='_blank'
+        ),
+    ]),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ ch4-layout/index.md
@@ -0,0 +1,8 @@
+# layouts if Dash apps
+
+- Hierarchical tree of components
+
+- Popular modules: 
+  - Dash HTML Components: `dash.html`
+  - Dash Core Components: `dash.dcc`
+
