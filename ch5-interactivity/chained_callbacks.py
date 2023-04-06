ab870e3829592b68672b806a6f030c2dab3022dc
Azat <8280770+azataiot@users.noreply.github.com>
2023-04-06 09:30:20 +0300

    ch5 chained_callbacks.py done

---
+++ ch5-interactivity/chained_callbacks.py
@@ -0,0 +1,93 @@
+from dash import Dash, dcc, html, Input, Output
+import pandas as pd
+
+# import the data from the csv file
+df = pd.read_csv('world_happiness.csv')
+
+app = Dash(__name__)
+
+title = html.H1('World Happiness Dashboard')
+description = html.P(
+    [
+        'This dashboard shows the happiness score for different countries',
+        html.Br(),
+        html.A('Data source', href='https://worldhappiness.report/'),
+    ]
+)
+
+# create the region radio buttons
+region_radio = dcc.RadioItems(
+    options=df['region'].unique(),
+    value='North America',
+)
+
+# create the dropdown
+dropdown = dcc.Dropdown(
+    options=df['country'].unique(),
+)
+
+# create the radio buttons
+radio = dcc.RadioItems(options={
+    'happiness_score': 'Happiness Score',
+    'happiness_rank': 'Happiness Rank',
+}, value='happiness_score')
+
+# create the figure
+graph = dcc.Graph()
+
+# create the div
+average = html.Div(
+    children=''
+)
+
+app.layout = html.Div([
+    title,
+    description,
+    region_radio,
+    dropdown,
+    radio,
+    graph,
+    average,
+])
+
+
+# create the callback for region selection
+@app.callback(
+    Output(dropdown, 'options'),
+    Input(region_radio, 'value'),
+)
+def update_dropdown(region):
+    return df[df['region'] == region]['country'].unique()
+
+
+# create the callback
+@app.callback(
+    Output(graph, 'figure'),
+    Output(average, 'children'),
+    Input(dropdown, 'value'),
+    Input(radio, 'value'),
+)
+def update_graph(country, metric):
+    # filter the data
+    country = country or 'United States'
+    df_filtered = df[df['country'] == country]
+    # create the figure
+    fig = {
+        'data': [
+            {
+                'x': df_filtered['year'],
+                'y': df_filtered[metric],
+                'type': 'line',
+            },
+        ],
+        'layout': {
+            'title': f'{metric} for {country}',
+        },
+    }
+    # create the average
+    avg = f'The average {metric} for {country} is {df_filtered[metric].mean():.2f}'
+    return fig, avg
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
